10df187ed634a8766b62322343c15a8d548dd3b9
Andres-MR <andrez.morales.r.7@gmail.com>
2024-05-18 08:56:02 -0500

    para la profe

---
+++ context/car/carState.js
@@ -0,0 +1,66 @@
+import React, {useReducer} from 'react'
+import CarContext from './carContext'
+import CarReducer from './carReducer'
+import { SELECT_VEHICLES, 
+    SAVE_ORDER,
+    DISPLAY_ORDER,
+    DELETE_ORDER
+} from '../../types'
+
+const CarState = props => {
+    const initialState = {
+        car:[],
+        buyVehicle: null
+    }
+
+    const[ state, dispatch] = useReducer(CarReducer, initialState)
+    //seleccionar y obtener el vehiculo
+    const selectVehicle = buyVehicle =>{
+        dispatch({
+            type: SELECT_VEHICLES,
+            payload: buyVehicle
+        })
+    }
+
+    //GUARDAR VEHICULO
+    const guardarVehiculo = car =>{
+        dispatch({
+            type: SAVE_ORDER,
+            payload: car
+        })
+    }
+
+    //mostrar los items del pedido
+    const mostrarResumen = total =>{
+        dispatch({
+            type: DISPLAY_ORDER,
+            payload: total
+        })
+    }
+
+    //eliminar articulo
+    const eliminarProducto = id =>{
+        dispatch({
+            type: DELETE_ORDER,
+            payload: id
+        })
+    }
+
+
+    return(
+        <CarContext.Provider
+            value={{
+                car: state.car,
+                buyVehicle: state.buyVehicle,
+                selectVehicle,
+                guardarVehiculo,
+                mostrarResumen,
+                eliminarProducto
+            }}
+        >
+            {props.children}
+        </CarContext.Provider>
+    )
+}
+
+export default CarState;
